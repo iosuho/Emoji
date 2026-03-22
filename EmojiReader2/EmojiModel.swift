bf10c52d1c3a9742a3c4484f4f2d4c99e2ba0bd2
Vladimir <proydakov99@bk.ru>
2026-03-22 14:18:13 +0300

    FirstCommit

---
+++ EmojiReader2/EmojiModel.swift
@@ -0,0 +1,15 @@
+//
+//  EmojiModel.swift
+//  EmojiReader2
+//
+//  Created by Владимир Пройдаков on 06.08.2024.
+//
+
+import Foundation
+
+struct Emoji {
+    var emoji: String
+    var name: String
+    var description: String
+    var isFavorite: Bool
+}
---
+++ EmojiReader2/EmojiTableViewCell.swift
@@ -0,0 +1,27 @@
+//
+//  EmojiTableViewCell.swift
+//  EmojiReader2
+//
+//  Created by Владимир Пройдаков on 05.08.2024.
+//
+
+import UIKit
+
+class EmojiTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var emojiLabel: UILabel!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+    }
+    
+    func set(object: Emoji) {
+        
+        self.emojiLabel.text = object.emoji
+        self.nameLabel.text = object.name
+        self.descriptionLabel.text = object.description
+    }
+}
